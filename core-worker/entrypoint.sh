5f5fb5b0ac9b4be5f98cce5be270eab55703b22f
Artur <git@artursterz.de>
2018-08-13 15:25:10 +0200

    cleaned up compose files; added core worker

---
+++ core-worker/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export GEOMETRY=${SCREEN_WIDTH}x${SCREEN_HEIGHT}x${SCREEN_DEPTH}
+x11vnc -storepasswd ${PASSWORD} ~/.vnc/passwd
+
+sleep 10
+
+python -u /worker/worker.py --backend maci-backend:63658 --capabilities core --no-clear-tmp-dir &
+
+exec /usr/bin/supervisord -n
+
