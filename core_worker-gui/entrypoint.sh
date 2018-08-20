6c88a0c7dd318d2d6f4d02b52e17df1024155987
Jonas Höchst <git@jonashoechst.de>
2018-08-20 18:05:45 +0200

    falling back to bash, if no maci-backend is available lxterminal (supports copy&paste instead of aterm)

--- core_worker-gui/entrypoint.sh
+++ core_worker-gui/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-core-daemon > /var/log/core-daemon.log &
+core-daemon > /var/log/core-daemon.log 2>&1 &
 sleep 1
 
-core-gui &
+core-gui > /var/log/core-gui.log 2>&1 &
 
 # connect to maci-backend or open a shell
-wget $BACKEND:63658/workers/script.py -O /worker/worker.py \
-    || bash
-
-python -u /worker/worker.py --backend $BACKEND:63658 --capabilities core --maxidletime $IDLE --no-clear-tmp-dir 
-
+wget $BACKEND:63658/workers/script.py -O /worker/worker.py &&
+    python -u /worker/worker.py --backend $BACKEND:63658 --capabilities core --maxidletime $IDLE --no-clear-tmp-dir &&
+    exit
+    
+echo "Couldn't connect to MACI backend, falling back to bash..." && bash
