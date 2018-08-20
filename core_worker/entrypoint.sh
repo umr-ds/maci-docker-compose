eec38c19e9b4eb4a271881fe8aa5eb534896fa4d
Artur <git@artursterz.de>
2018-08-20 13:34:37 +0200

    cleaned up core worker

--- core_worker/entrypoint.sh
+++ core_worker/entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-export GEOMETRY=${SCREEN_WIDTH}x${SCREEN_HEIGHT}x${SCREEN_DEPTH}
-x11vnc -storepasswd ${PASSWORD} ~/.vnc/passwd
-
 sleep 10
 
 wget $BACKEND:63658/workers/script.py -O /worker/worker.py
-python -u /worker/worker.py --backend $BACKEND:63658 --capabilities core --maxidletime $IDLE --no-clear-tmp-dir &
-
-exec /usr/bin/supervisord -n
-
+python -u /worker/worker.py --backend $BACKEND:63658 --capabilities core --maxidletime $IDLE --no-clear-tmp-dir
