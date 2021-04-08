4ea50951ad34323df00d8e9587b13272c2252340
Markus Sommer <msommer@informatik.uni-marburg.de>
2021-04-08 18:45:57 +0200

    Update ns3_worker

--- ns3_worker/entrypoint.sh
+++ ns3_worker/entrypoint.sh
@@ -3,4 +3,4 @@
 sleep 10
 
 wget $BACKEND:63658/workers/script.py -O /worker/worker.py
-python -u /worker/worker.py --backend $BACKEND:63658 --capabilities ns3 --maxidletime $IDLE --no-clear-tmp-dir
+python3 -u /worker/worker.py --backend $BACKEND:63658 --capabilities ns3 --maxidletime $IDLE --no-clear-tmp-dir
